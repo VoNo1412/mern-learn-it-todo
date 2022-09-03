1ae882c662e776845c610d016c0217226c0d097d
vono <xkaioken9x@gmail.com>
2022-09-03 22:59:21 +0700

    update code client

--- client/src/context/constant.js
+++ client/src/context/constant.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // export const apiURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : '';
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/api'
+    baseURL: process.env.NODE_ENV !== "production" ? 'https://desolate-chamber-93177.herokuapp.com/api' :  'http://localhost:5000/api'
 })
 export const LOCAL_STORAGE_TOKEN = 'learn-it';
